6ebc75bc5702678663ba567eccc3f87fce4b5ca8
Eric Shinn <eshinn@madmobile.com>
2016-05-16 10:27:10 -0400

    Split build process in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
+
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+
+const common = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter from given it's conventient with more complex configuration.
   entry: {
@@ -16,3 +20,12 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+// Default configuration. We will return this if
+// Webpack is called outside of npm.
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
